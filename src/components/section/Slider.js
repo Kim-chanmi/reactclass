1296281c23e14d19a574a4acc22ebb665576d872
Kim-chanmi <chanmi1320@gmail.com>
2022-10-25 13:19:27 +0900

    2022.10.25

--- src/components/section/Slider.js
+++ src/components/section/Slider.js
@@ -19,48 +19,48 @@ function Slider() {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div class="desc">
+          <div className="desc">
             <span>PLANT</span>
             <h3>반려식물 키우기</h3>
             <p>
               반려식물 혼자 키우기 힘드시죠? <br />
               우리가 당신과 함께하겠습니다.
             </p>
-            <div class="btn">
-              <a href="#">자세히 보기</a>
-              <a href="#" class="black">
+            <div className="btn">
+              <a href="/">자세히 보기</a>
+              <a href="/" className="black">
                 사이트 보기
               </a>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div class="desc">
+          <div className="desc">
             <span>PLANT</span>
             <h3>반려식물 키우기</h3>
             <p>
               반려식물 혼자 키우기 힘드시죠? <br />
               우리가 당신과 함께하겠습니다.
             </p>
-            <div class="btn">
-              <a href="#">자세히 보기</a>
-              <a href="#" class="black">
+            <div className="btn">
+              <a href="/">자세히 보기</a>
+              <a href="/" className="black">
                 사이트 보기
               </a>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div class="desc">
+          <div className="desc">
             <span>PLANT</span>
             <h3>반려식물 키우기</h3>
             <p>
               반려식물 혼자 키우기 힘드시죠? <br />
               우리가 당신과 함께하겠습니다.
             </p>
-            <div class="btn">
-              <a href="#">자세히 보기</a>
-              <a href="#" class="black">
+            <div className="btn">
+              <a href="/">자세히 보기</a>
+              <a href="/" className="black">
                 사이트 보기
               </a>
             </div>
